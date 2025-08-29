8816f65277298b4e3fc18ca21e4338371d183a66
Nabil Amin Hridoy <66374353+nabilaminhridoy@users.noreply.github.com>
2025-08-29 17:17:48 +0600

    Initial commit

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -85,7 +85,7 @@ export const ContactSection = () => {
                   <MapPin className="h-6 w-6 text-primary" />{" "}
                 </div>
                 <div>
-                  <h4 className="font-medium"> Location</h4>
+                  <h4 className="font-medium text-left"> Location</h4>
                   <a className="text-muted-foreground hover:text-primary transition-colors">
                     240/1, South Paikpara, Mirpur, Dhaka
                   </a>
